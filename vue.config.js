c7ed57f1fb15c287c65aea386cb0479a29850697
BobrikovAleksey <bobrikov.spb@ya.ru>
2021-03-01 12:18:12 +0300

    Add proxy   (#11)

---
+++ vue.config.js
@@ -0,0 +1,32 @@
+const serverUrl = 'http://menly.1site.space';
+
+module.exports = {
+  devServer: {
+    https: true,
+    proxy: {
+      '^/api*': {
+        "target": serverUrl,
+        ws: true,
+        changeOrigin: true
+      },
+      '^/auth*': {
+        "target": serverUrl,
+        ws: true,
+        changeOrigin: true
+      },
+      '^/sms*': {
+        "target": serverUrl,
+        ws: true,
+        changeOrigin: true
+      },
+      '^/img*': {
+        "target": serverUrl,
+        ws: true,
+        changeOrigin: true
+      },
+    },
+  },
+  'transpileDependencies': [
+    'vuetify',
+  ],
+};
